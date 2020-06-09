5b95dcd92dd9444580810932f3ac6dafbfce9787
Sushanta Ray <52099579+SushantaRay7@users.noreply.github.com>
2020-06-09 15:08:27 +0530

    Create 9_Is Subsequence.java

---
+++ 9_Is Subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int m= s.length();
+        int n=t.length();
+        
+        return subsequence(s,t,m,n);
+        
+    }
+    public boolean subsequence(String s, String t , int m, int n){
+        if(m==0)
+            return true;
+        if(n==0)
+            return false;
+        if(s.charAt(m-1)==t.charAt(n-1)){
+            return subsequence(s,t,m-1,n-1);
+        }
+        return subsequence(s,t,m,n-1);
+    }
+}
